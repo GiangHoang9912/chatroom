2a2e92a85a08540a37b7639ebee05729cc6b039e
Giang Hoang <gianghoang.9955@gmail.com>
2020-04-29 11:40:37 +0700

    --- Auto-Git Commit ---

---
+++ DBcontext.js
@@ -0,0 +1,53 @@
+let express = require('express')
+
+let app = express()
+
+app.use(express.static("public"))
+
+app.set("view engine", "ejs")
+
+app.set("views", "./views")
+
+app.listen(3000)
+
+const sql = require('mssql')
+const config = {
+    user : "sa",
+    password : "sa",
+    database : "MariaCoffee",
+    server : "localhost",
+    port : 1433
+}
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Connected to MSSQL')
+    return pool
+  })
+  .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
+
+
+app.get('/', async (req, res) => {
+  try {
+    const pool = await poolPromise
+    const result = await pool.request().query('select * from Intro')
+
+    res.render("Mssql", {Intro : result})
+
+    console.log(result)
+  } catch (err) {
+
+    res.status(500)
+    res.send(err.message)
+  }
+})
+
+let s = {
+    1 : "a",
+    2 : "b",
+    3 : "c",
+    4 : "d"
+}
+
+console.log(1)
